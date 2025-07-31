f3d720d7348192b9afc13642015483ec7fd2fd83
Nithishkumar-T <109725053+Nithishkumar-T@users.noreply.github.com>
2025-07-31 12:48:59 +0530

    Update CcspTandD_XLE_SelfHeal.cpp

--- source/test/CcspTandD_Dml_Test/CcspTandD_XLE_SelfHeal.cpp
+++ source/test/CcspTandD_Dml_Test/CcspTandD_XLE_SelfHeal.cpp
@@ -20,9 +20,9 @@
 #include <gtest/gtest.h>
 #include "CcspTandD_Dml_Mock.h"
 
-extern "C" {
+/*extern "C" {
 
-/*struct xle_attributes
+struct xle_attributes
 {
   int devicemode;
   int is_lte_wan_up;
@@ -41,10 +41,10 @@ extern "C" {
   int is_default_route;
   int is_mesh_default_route;
   int cellular_restart_count;
-}xle_attributes;*/
+}xle_attributes;
 
 extern struct xle_attributes xle_params;
-}
+}*/
 
 class CcspTandD_XLE_SelfHealTest : public ::testing::Test {
 protected:
