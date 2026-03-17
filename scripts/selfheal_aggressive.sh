2af4ff74bfb98694d82aea56c63d5e44722c75d6
aj970-crypto <akshaya_j@comcast.com>
2026-03-18 00:19:32 +0530

    RDKB-63415 : Move SelfHeal Scripts to Cron (#143)
    RDKB-63415 : Move SelfHeal Scripts to Cron
    
    Reason for change: Selfheal scripts must be running in process mode when
    the syscfg get returns NULL and default param value must be set to
    false.
    Test Procedure: Check scripts are running in process mode when syscfg
    get returns NULL
    Risks: Low
    Priority: P0
    
    ---------
    
    Signed-off-by: aj970 <akshaya_j@comcast.com>

--- scripts/selfheal_aggressive.sh
+++ scripts/selfheal_aggressive.sh
@@ -1742,22 +1742,23 @@ DHCP_Selfheal() {
 }
 
 cron_mode()
-{
+{   
+    acquire_lock "selfheal_aggressive" "selfheal_aggressive.sh"
 	echo_t "[RDKB_AGG_SELFHEAL] : Cron job is enabled"
 	# skip during boot of first 5 minutes
 	BOOTUP_TIME_SEC=$(cut -d. -f1 /proc/uptime)
 	if [ "$BOOTUP_TIME_SEC" -le 300 ]; then
             echo_t "[RDKB_AGG_SELFHEAL] : Still booting, skipping"
             exit 0
-        fi
+    fi
 
 	if [ "$(syscfg get selfheal_enable)" != "true" ]; then
             echo_t "[RDKB_SELFHEAL] : selfheal_enable != true, exiting"
             exit 0
-        fi
+    fi
         
-        DHCP_Selfheal
-        exit 0
+    DHCP_Selfheal
+    exit 0
 }
 
 process_mode()
@@ -1773,10 +1774,8 @@ process_mode()
         done
 }
 
-CRON_ENABLED=$(syscfg get SelfHealCronEnable)
-
-if [ "$CRON_ENABLED" = "true" ]; then
-	cron_mode
+if [ "$SELFHEAL_EXECUTION_MODE" = "CRON" ]; then
+    cron_mode
 else
-	process_mode
+    process_mode
 fi
