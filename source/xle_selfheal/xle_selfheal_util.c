4a7e050b6d871890febd618d6e5f80899f900329
Harnish Patel <163233851+hpatel006c@users.noreply.github.com>
2026-03-26 16:05:30 -0700

    Update xle_selfheal_util.c

--- source/xle_selfheal/xle_selfheal_util.c
+++ source/xle_selfheal/xle_selfheal_util.c
@@ -197,7 +197,7 @@ void PopulateParameters()
     {
         xle_log("CCSP_Message_Bus_Init failed for component %s: %d\n", component_id, ret);
         bus_handle = NULL;
-        exit(1);
+        //exit(1);
     }
     int retPsmGet = PSM_Get_Record_Value2(bus_handle,g_Subsystem, "dmsb.Mesh.WAN.Interface.Name", NULL, &paramValue);
     if (retPsmGet == CCSP_SUCCESS)
