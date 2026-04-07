22221f653c6ebd648144736a4d2fc812e5560b48
Nithishkumar-T <109725053+Nithishkumar-T@users.noreply.github.com>
2026-04-07 16:27:27 +0530

    Update CcspTandD_XLE_SelfHeal.cpp

--- source/test/CcspTandD_Dml_Test/CcspTandD_XLE_SelfHeal.cpp
+++ source/test/CcspTandD_Dml_Test/CcspTandD_XLE_SelfHeal.cpp
@@ -1,29 +1,9 @@
-/*
-* If not stated otherwise in this file or this component's LICENSE
-* file the following copyright and licenses apply:
-*
-* Copyright 2015 RDK Management
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 #include <gtest/gtest.h>
 #include "CcspTandD_Dml_Mock.h"
 
 extern "C" {
 
-struct xle_attributes
-{
+struct xle_attributes {
     int devicemode;
     int is_lte_wan_up;
     int iswan_dhcp_client;
@@ -47,24 +27,9 @@ extern struct xle_attributes xle_params;
 void PopulateParameters(void);
 }
 
-/*
- * NOTE:
- * Mocks are created globally in CcspTandD_Dml_Mock.cpp.
- * Do NOT allocate or delete them here.
- */
-class CcspTandD_XLE_SelfHealTest : public ::testing::Test
-{
+class CcspTandD_XLE_SelfHealTest : public ::testing::Test {
 protected:
-    void SetUp() override
-    {
-        ::testing::Mock::VerifyAndClearExpectations(g_libnetMock);
-        ::testing::Mock::VerifyAndClearExpectations(g_messagebusMock);
-        ::testing::Mock::VerifyAndClearExpectations(g_psmMock);
-        ::testing::Mock::VerifyAndClearExpectations(g_baseapiMock);
-    }
-
-    void TearDown() override
-    {
+    void SetUp() override {
         ::testing::Mock::VerifyAndClearExpectations(g_libnetMock);
         ::testing::Mock::VerifyAndClearExpectations(g_messagebusMock);
         ::testing::Mock::VerifyAndClearExpectations(g_psmMock);
@@ -74,9 +39,9 @@ protected:
 
 TEST_F(CcspTandD_XLE_SelfHealTest, test_PopulateParameters)
 {
+    /* prepare parameter values */
     parameterValStruct_t **parameterVal =
-        static_cast<parameterValStruct_t **>(malloc(4 * sizeof(parameterValStruct_t *)));
-    ASSERT_NE(parameterVal, nullptr);
+        (parameterValStruct_t **)calloc(4, sizeof(parameterValStruct_t*));
 
     const char* names[] = {
         "Device.Cellular.X_RDK_Status",
@@ -84,33 +49,23 @@ TEST_F(CcspTandD_XLE_SelfHealTest, test_PopulateParameters)
         "Device.Cellular.Interface.1.Enable",
         "Device.Cellular.Interface.1.X_RDK_ContextProfile.1.IpAddressFamily"
     };
+    const char* values[] = { "CONNECTED", "true", "true", "IPv4" };
 
-    const char* values[] = {
-        "CONNECTED",
-        "true",
-        "true",
-        "IPv4"
-    };
-
-    for (int i = 0; i < 4; ++i)
-    {
-        parameterVal[i] = static_cast<parameterValStruct_t *>(malloc(sizeof(parameterValStruct_t)));
-        ASSERT_NE(parameterVal[i], nullptr);
-
+    for (int i = 0; i < 4; i++) {
+        parameterVal[i] = (parameterValStruct_t*)calloc(1, sizeof(parameterValStruct_t));
         parameterVal[i]->parameterName = strdup(names[i]);
         parameterVal[i]->parameterValue = strdup(values[i]);
     }
 
-    EXPECT_CALL(*g_messagebusMock, CCSP_Message_Bus_Init(_, _, _, _, _))
-        .Times(::testing::AtLeast(1))
+    EXPECT_CALL(*g_messagebusMock,
+        CCSP_Message_Bus_Init(_, _, _, _, _))
         .WillOnce(::testing::Return(CCSP_SUCCESS));
 
     EXPECT_CALL(*g_baseapiMock,
         CcspBaseIf_getParameterValues(_, _, _, _, _, _, _))
-        .Times(1)
         .WillOnce(::testing::DoAll(
-            ::testing::WithArg<6>([&](parameterValStruct_t ***outComponents) {
-                *outComponents = parameterVal;
+            ::testing::WithArgs<6>([&](parameterValStruct_t*** out) {
+                *out = parameterVal;
             }),
             ::testing::Return(100)
         ));
@@ -120,47 +75,30 @@ TEST_F(CcspTandD_XLE_SelfHealTest, test_PopulateParameters)
         .Times(1);
 
     EXPECT_CALL(*g_psmMock,
-        PSM_Get_Record_Value2(_, _, ::testing::StrEq("dmsb.Mesh.WAN.Interface.Name"), _, _))
-        .Times(1)
-        .WillOnce(::testing::DoAll(
-            ::testing::SetArgPointee<4>(nullptr),
-            ::testing::Return(CCSP_FAILURE)
-        ));
+        PSM_Get_Record_Value2(_, _, _, _, _))
+        .WillOnce(::testing::Return(CCSP_FAILURE));
 
     EXPECT_CALL(*g_libnetMock,
         interface_status(_, _))
-        .Times(1)
         .WillOnce(::testing::DoAll(
             ::testing::SetArgPointee<1>(1),
             ::testing::Return(CNL_STATUS_SUCCESS)
         ));
 
-    xle_params.devicemode = 1;
-
     EXPECT_CALL(*g_libnetMock,
         get_ipv6_address(_, _, _))
-        .Times(1)
         .WillOnce(::testing::DoAll(
-            ::testing::Invoke([](auto, char* out_ipv6, auto) {
-                strcpy(out_ipv6, "2001:db8:85a3:8d3:1319:8a2e:370:7348");
+            ::testing::Invoke([](const char*, char* out, size_t) {
+                strcpy(out, "2001:db8::1");
             }),
             ::testing::Return(CNL_STATUS_SUCCESS)
         ));
 
-    char ip_address[] = "10.32.43.4";
     EXPECT_CALL(*g_libnetMock,
         interface_get_ip(_))
-        .Times(1)
-        .WillOnce(::testing::Return(ip_address));
+        .WillOnce(::testing::Return((char*)"10.32.43.4"));
+
+    xle_params.devicemode = 1;
 
     PopulateParameters();
-
-    /* cleanup test allocations */
-    for (int i = 0; i < 4; ++i)
-    {
-        free(parameterVal[i]->parameterName);
-        free(parameterVal[i]->parameterValue);
-        free(parameterVal[i]);
-    }
-    free(parameterVal);
 }
