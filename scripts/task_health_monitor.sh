005d07e35fb1af1054097d91b06d3d5fb972e34a
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-08-12 14:50:42 +0530

    Update task_health_monitor.sh

--- scripts/task_health_monitor.sh
+++ scripts/task_health_monitor.sh
@@ -4146,11 +4146,19 @@ case $SELFHEAL_TYPE in
     "BASE")
         #Checking the ntpd is running or not
         if [ "$WAN_TYPE" != "EPON" ]; then
-            NTPD_PID=$(busybox pidof ntpd)
-            if [ "$NTPD_PID" = "" ]; then
-                echo_t "RDKB_PROCESS_CRASHED : NTPD is not running, restarting the NTPD"
-                sysevent set ntpd-restart
-            fi
+           if [ "$(syscfg get chrony_enabled)" = "true" ]; then
+		        CHRONYD_PID=$(busybox pidof chronyd)
+                if [ "$CHRONYD_PID" = "" ]; then
+                    echo_t "RDKB_PROCESS_CRASHED : chronyd is not running, restarting chronyd"
+                    sysevent set chronyd-restart
+                fi
+		  else
+                NTPD_PID=$(busybox pidof ntpd)
+                if [ "$NTPD_PID" = "" ]; then
+                    echo_t "RDKB_PROCESS_CRASHED : NTPD is not running, restarting the NTPD"
+                    sysevent set ntpd-restart
+                fi
+		  fi
 
 
             #Checking if rpcserver is running
@@ -4164,6 +4172,19 @@ case $SELFHEAL_TYPE in
     "TCCBR")
         #Checking the ntpd is running or not for TCCBR
         if [ "$WAN_TYPE" != "EPON" ]; then
+		  if [ "$(syscfg get chrony_enabled)" = "true" ]; then
+		        CHRONYD_PID=$(busybox pidof chronyd)
+                if [ "$CHRONYD_PID" = "" ]; then
+                    echo_t "RDKB_PROCESS_CRASHED : chronyd is not running, restarting chronyd"
+                    sysevent set chronyd-restart
+                fi
+		  else
+                NTPD_PID=$(busybox pidof ntpd)
+                if [ "$NTPD_PID" = "" ]; then
+                    echo_t "RDKB_PROCESS_CRASHED : NTPD is not running, restarting the NTPD"
+                    sysevent set ntpd-restart
+                fi
+		 fi
             NTPD_PID=$(busybox pidof ntpd)
             if [ "$NTPD_PID" = "" ]; then
                 echo_t "RDKB_PROCESS_CRASHED : NTPD is not running, restarting the NTPD"
