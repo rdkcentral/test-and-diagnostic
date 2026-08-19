e44b666e5a668ae86abb709e7aacf869427f839a
Chelliah <schell041@apac.comcast.com>
2026-08-19 15:22:44 +0530

    Adding debug prints

--- source/dmltad/cosa_wanconnectivity_rbus_handler_apis.c
+++ source/dmltad/cosa_wanconnectivity_rbus_handler_apis.c
@@ -972,6 +972,8 @@ rbusError_t WANCNCTVTYCHK_StartConnectivityCheck(rbusHandle_t handle, char const
     PWANCNCTVTY_CHK_GLOBAL_INTF_INFO gIntfInfo = NULL;
     ANSC_STATUS returnStatus = ANSC_STATUS_SUCCESS;
 
+    WANCHK_LOG_INFO("%s: Enter - method=%s\n", __FUNCTION__, methodName);
+
     value = rbusObject_GetValue(inParams, "linux_interface_name");
     if (value == NULL) {
         WANCHK_LOG_ERROR("%s: Missing required param: linux_interface_name\n", __FUNCTION__);
